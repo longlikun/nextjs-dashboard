aec87d55ec08cb9b480b04872195fe729cae3b4b
longlikun6 <longlikun6@gmail.com>
2024-07-25 15:59:24 +0800

    seed products

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,10 +1,10 @@
 'use server';
- 
+
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
- 
+
 // const FormSchema = z.object({
 //   id: z.string(),
 //   customerId: z.string(),
@@ -27,18 +27,39 @@ const FormSchema = z.object({
   date: z.string(),
 });
 
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
+
 export async function createInvoice(prevState: State, formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+
+  const validatedFields = CreateInvoice.safeParse({
+    
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
- 
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+
+  // Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
- 
+  
   try {
     await sql`
       INSERT INTO invoices (customer_id, amount, status, date)
@@ -49,25 +70,25 @@ export async function createInvoice(prevState: State, formData: FormData) {
       message: 'Database Error: Failed to Create Invoice.',
     };
   }
- 
+
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
-  // Use Zod to update the expected types
+// Use Zod to update the expected types
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
- 
+
 // ...
- 
+
 export async function updateInvoice(id: string, formData: FormData) {
   const { customerId, amount, status } = UpdateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
- 
+
   const amountInCents = amount * 100;
- 
+
   try {
     await sql`
         UPDATE invoices
@@ -77,7 +98,7 @@ export async function updateInvoice(id: string, formData: FormData) {
   } catch (error) {
     return { message: 'Database Error: Failed to Update Invoice.' };
   }
- 
+
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
@@ -92,11 +113,3 @@ export async function deleteInvoice(id: string) {
   }
 }
 
-export type State = {
-  errors?: {
-    customerId?: string[];
-    amount?: string[];
-    status?: string[];
-  };
-  message?: string | null;
-};
