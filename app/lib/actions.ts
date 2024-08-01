5a7ab871d9585e04f4eedaad1716ff070e55d435
longlikun6 <longlikun6@gmail.com>
2024-08-01 18:14:24 +0800

    fix: add env

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 // const FormSchema = z.object({
 //   id: z.string(),
@@ -59,7 +61,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
   const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
-  
+
   try {
     await sql`
       INSERT INTO invoices (customer_id, amount, status, date)
@@ -113,3 +115,22 @@ export async function deleteInvoice(id: string) {
   }
 }
 
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
